9d925a8cf7193466943e33ea04d5f68978942b29
Laura Trager <laura.trager@gmail.com>
2017-10-31 01:50:04 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ end
 calls = ["two", "go", "industrious", "bopsie"]
 
 def long_planeteer_calls(calls)
-  calls.any?{|word| word.length > 4}  
+  calls.any?{|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
